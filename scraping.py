43a36daf560d0f0bdc5ee19485b9942ecc8001d2
amakarewicz <a.makarewicz@student.mini.pw.edu.pl>
2021-12-16 23:52:41 +0100

    Artists ids

--- scraping.py
+++ scraping.py
@@ -1,9 +1,4 @@
 # %%
-import csv
-import requests
-import os
-import gzip
-import shutil
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -15,36 +10,24 @@ soup=BeautifulSoup(html, "html.parser")
 
 # %%
 ids = ""
-i=0
+#links = ""
+i=1
 for item in soup.find_all('td', attrs={'class' : 'text'}):
-    if i <=10:
-        link = item.div.a['href'].split("/")[1].split(".")[0]
-        # print(link)
-        ids += link + "%"
+    id = item.div.a['href'].split("/")[1].split(".")[0]
+    ids += id + ","
+    if i == 5000:
+        ids = ids[:-1]
+        break
+    if i % 40 == 0:
+        ids = ids[:-1] + "\n"
     i += 1
-print(ids[:-1])
-
-
-# for item in soup.find_all('h2'):
-#     string=item.get_text()
-#     split_string=string.split(", ")
-#     country=split_string[-1]
-#     if country in countries:
-#         print(string)
-
+    # albums_link = "https://api.spotify.com/v1/artists/" + id + "/albums"
+    # links += albums_link + "\n"
+# links=links[:-1]
 
 # %%
-# for item in soup.find_all('h2'):
-#     string=item.get_text()
-#     split_string=string.split(', ')
-#     country=split_string[-1]
-#     if country in countries:
-#         table=item.nextSibling.nextSibling.nextSibling.nextSibling
-#         #print(table.get('class', []))
-#         #print(type(table))
-#         listing_url=table.contents[3].contents[1].contents[5].contents[0]['href']
-#         filename=f'airbnb_gz\{string}.csv.gz'
+with open("artists_ids.txt", "w") as output:
+    output.write(ids)
+# with open("albums_links.txt", "w") as output:
+#     output.write(links)
 
-#         with open(filename, "wb") as f:
-#             r = requests.get(listing_url)
-#             f.write(r.content)
